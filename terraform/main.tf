835c7398bd9b241955562c27f42dd24a835e6eb4
Alex A <alexamog@outlook.com>
2023-03-16 19:12:41 -0700

    Made changes to code

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,3 +136,34 @@ resource "aws_security_group" "acit_sg_rds" {
   }
 
 }
+
+resource "aws_key_pair" "assign3-keypair" {
+  key_name   = "assign3-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "assign3-key"
+
+  tags = {
+    Name = "assign3-instance"
+  }
+}
